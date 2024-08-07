292a856f547c07cfd97978535c0f8c848f96fb7d
John Moscoso <johnmoscoso911@gmail.com>
2024-08-07 11:31:22 -0500

    Camel

--- camel-app/src/main/java/ec/com/eurofish/MyRoutes.java
+++ camel-app/src/main/java/ec/com/eurofish/MyRoutes.java
@@ -3,52 +3,57 @@ package ec.com.eurofish;
 import javax.inject.Inject;
 
 import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.cdi.Uri;
+import org.apache.camel.component.http.HttpMethods;
+import org.apache.camel.model.dataformat.JsonLibrary;
+
+import ec.com.eurofish.model.Message;
 
 /**
  * Configures all our Camel routes, components, endpoints and beans
  */
 public class MyRoutes extends RouteBuilder {
 
-    @Inject
-    @Uri("timer:foo?period=5000")
-    private Endpoint inputEndpoint;
+        @Inject
+        @Uri("timer:foo?period=5000")
+        private Endpoint inputEndpoint;
 
-    @Inject
-    @Uri("elasticsearch://elasticsearch?hostAddresses=integrador.eurofish.com.ec:9200&operation=Index&indexName=haccp")
-    private Endpoint resultEndpoint;
+        @Inject
+        @Uri("elasticsearch://elasticsearch?hostAddresses=integrador.eurofish.com.ec:9200&operation=Index&indexName=haccp")
+        private Endpoint resultEndpoint;
 
-    @Inject
-    @Uri("https://integrador.eurofish.com.ec:8490/v1/api/message/generic")
-    private Endpoint genericEndPoint;
+        @Inject
+        @Uri("https://integrador.eurofish.com.ec:8490/v1/api/message/generic")
+        private Endpoint genericEndPoint;
 
-    @Inject
-    @Uri("paho-mqtt5:blz/haccp?brokerUrl=ssl://integrador.eurofish.com.ec:8883&userName=mosquitto&password=mosquitto")
-    private Endpoint haccp;
+        @Inject
+        @Uri("paho-mqtt5:blz/haccp?brokerUrl=ssl://integrador.eurofish.com.ec:8883&userName=mosquitto&password=mosquitto")
+        private Endpoint haccp;
 
-    @Override
-    public void configure() {
-        // you can configure the route rule with Java DSL here
+        @Override
+        public void configure() {
+                // you can configure the route rule with Java DSL here
 
-        from(inputEndpoint)
-                .to("bean:counterBean")
-                .to("log:output")
-                .to(resultEndpoint)
-                .log("${body}");
+                // from(inputEndpoint)
+                // .to("bean:counterBean")
+                // .to("log:output")
+                // .to(resultEndpoint)
+                // .log("${body}");
 
-        from(haccp)
-                // .log("${body}")
-                .to("log:output");
+                from(haccp)
+                                // .log("${body}")
+                                .to("log:output");
 
-        from(inputEndpoint)
-                .process(exchange -> exchange.getIn().setBody(Message.plannedAssistance()))
-                .marshal().json(JsonLibrary.Jackson)
-                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
-                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-                .log("${body}")
-                .to(genericEndPoint)
-                .log("${body}");
-    }
+                from(inputEndpoint)
+                                .process(exchange -> exchange.getIn().setBody(Message.test()))
+                                .marshal().json(JsonLibrary.Jackson)
+                                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
+                                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+                                .log("${body}")
+                                .to(genericEndPoint)
+                                .log("${body}");
+        }
 
 }
