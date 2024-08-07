ed3ceedbbfd8431b4f36fd2080b988cc10b26210
John Moscoso <johnmoscoso911@gmail.com>
2024-08-06 19:52:05 -0500

    camel

---
+++ camel-app/src/main/java/ec/com/eurofish/MyRoutes.java
@@ -0,0 +1,33 @@
+package ec.com.eurofish;
+
+import javax.inject.Inject;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.cdi.Uri;
+
+/**
+ * Configures all our Camel routes, components, endpoints and beans
+ */
+public class MyRoutes extends RouteBuilder {
+
+    @Inject
+    @Uri("timer:foo?period=5000")
+    private Endpoint inputEndpoint;
+
+    @Inject
+    @Uri("elasticsearch://elasticsearch?hostAddresses=integrador.eurofish.com.ec:9200&operation=Index&indexName=haccp")
+    private Endpoint resultEndpoint;
+
+    @Override
+    public void configure() {
+        // you can configure the route rule with Java DSL here
+
+        from(inputEndpoint)
+                .to("bean:counterBean")
+                .to("log:output")
+                .to(resultEndpoint)
+                .log("${body}");
+    }
+
+}
